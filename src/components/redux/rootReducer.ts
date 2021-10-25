f2c2ffc98dee344bc1c28621dbdf498c723e2f0d
ga-rajeshbj <rajesh.bj@green-apex.com>
2021-10-25 13:01:04 +0530

    initial commit

--- src/components/redux/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { fechingReducer } from "./reducer/reducer";
-
-export const rootReducer = combineReducers({ fechingReducer });
-
-export type AppState = ReturnType<typeof rootReducer>;
