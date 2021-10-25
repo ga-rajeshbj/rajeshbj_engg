85719fb523cde87e3710d0ff70c2b0e5b1116ab0
ga-rajeshbj <rajesh.bj@green-apex.com>
2021-10-25 19:35:40 +0530

    task completed

---
+++ nasa-app/src/components/AsteroidInformation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import { Location } from "./Types";
+function AsteroidInformation() {
+  const location: Location = useLocation();
+
+  const { nasa_jpl_url, is_potentially_hazardous_asteroid, name } =
+    location.state;
+
+  return (
+    <div className="d-flex align-items-center justify-content-center flex-column">
+      <h1 className="text-success">Asteroid Details</h1>
+      <Card className="mt-3" style={{ width: "45rem" }}>
+        <Card.Body>
+          <Card.Title className="text-primary">Name: {name}</Card.Title>
+          <Card.Text className="text-secondary">
+            nasa_jpl_url : {nasa_jpl_url}
+          </Card.Text>
+          <Card.Text className="text-secondary">
+            is_potentially_hazardous_asteroid:{" "}
+            {is_potentially_hazardous_asteroid ? "Yes" : "No"}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default AsteroidInformation;
---
+++ nasa-app/src/container/FindingForm.tsx
@@ -0,0 +1,92 @@
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AstroidData } from "../components/Types";
+
+function FindingForm() {
+  const [asteroidID, setAsteroidID] = useState<null | number>(null);
+
+  const handleIdChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setAsteroidID(Number(event.target.value));
+  };
+  const url = `https://api.nasa.gov/neo/rest/v1/neo/${asteroidID}?api_key=l9gVhCIXkd0fU9fY4Zxy7aK0aqNrAitrJAep5ZjA`;
+
+  let history = useHistory();
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    getData(url);
+  };
+
+  const getRamdomAstroid = async () => {
+    const reandomUrl =
+      "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=l9gVhCIXkd0fU9fY4Zxy7aK0aqNrAitrJAep5ZjA";
+
+    let response: any = await axios.get(reandomUrl);
+    let astroids = response.data.near_earth_objects;
+    let id = astroids[Math.floor(Math.random() * astroids.length)].id;
+    const url = `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=l9gVhCIXkd0fU9fY4Zxy7aK0aqNrAitrJAep5ZjA`;
+    getData(url);
+  };
+
+  const getData = async (url: string) => {
+    await axios
+      .get<AstroidData>(url)
+      .then((response) => {
+        const { id, nasa_jpl_url, is_potentially_hazardous_asteroid, name } =
+          response.data;
+
+        history?.push({
+          pathname: `./astroidDetails/${id}/${name}`,
+          state: { nasa_jpl_url, is_potentially_hazardous_asteroid, name },
+        });
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="text-success">Find The Asteroider Using id</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <TextField
+          id="outlined-basic"
+          label="Enter Asteroid ID"
+          variant="standard"
+          placeholder="Enter Asteroid ID"
+          size="small"
+          required
+          onChange={handleIdChange}
+          type="number"
+        />
+        <br></br>
+        <Button
+          type="submit"
+          disabled={asteroidID ? false : true}
+          color="primary"
+          variant="contained"
+          className="mt-2"
+        >
+          submit
+        </Button>
+      </form>
+
+      <div>
+        {" "}
+        <Button
+          onClick={() => getRamdomAstroid()}
+          color="secondary"
+          variant="contained"
+          className="mt-5"
+        >
+          Rondom Astroid
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default FindingForm;
